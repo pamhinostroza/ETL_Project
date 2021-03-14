fa394ab2af46d3d701f06bb58d7bd1477ff54650
pamhinostroza <toxicmrmade@yahoo.com>
2021-03-14 00:05:55 -0500

    added resources and output folders plus ppt sql and pandas files

---
+++ Resources/QuickDBD_export_sql_code.sql
@@ -0,0 +1,39 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "cost_living_index" (
+    "State" VARCHAR   NOT NULL,
+    "costIndex" INT   NOT NULL,
+    "groceryCost" INT   NOT NULL,
+    "housingCost" INT   NOT NULL,
+    "utilitiesCost" INT   NOT NULL,
+    "transportationCost" INT   NOT NULL,
+    "miscCost" INT   NOT NULL,
+    CONSTRAINT "pk_cost_living_index" PRIMARY KEY (
+        "State"
+     )
+);
+
+CREATE TABLE "median_home_prices" (
+    "State" VARCHAR   NOT NULL,
+    "MedianValue" INT   NOT NULL,
+    CONSTRAINT "pk_median_home_prices" PRIMARY KEY (
+        "MedianValue"
+     )
+);
+
+CREATE TABLE "median_household_income" (
+    "State" VARCHAR   NOT NULL,
+    "HouseholdIncome" INT   NOT NULL
+);
+
+ALTER TABLE "median_home_prices" ADD CONSTRAINT "fk_median_home_prices_State" FOREIGN KEY("State")
+REFERENCES "cost_living_index" ("State");
+
+ALTER TABLE "median_household_income" ADD CONSTRAINT "fk_median_household_income_State" FOREIGN KEY("State")
+REFERENCES "cost_living_index" ("State");
+
+ALTER TABLE "median_household_income" ADD CONSTRAINT "fk_median_household_income_HouseholdIncome" FOREIGN KEY("HouseholdIncome")
+REFERENCES "median_home_prices" ("MedianValue");
+
---
+++ Resources/config_indeed.py
@@ -0,0 +1,2 @@
+#  Add your API key
+api_key_i = "YOUR KEY HERE!"
