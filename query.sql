d3a4f933cfaf222940e148d25e70d23dc2506271
pamhinostroza <toxicmrmade@yahoo.com>
2021-03-16 22:49:54 -0400

    everything done except powerpoint

--- query.sql
+++ query.sql
@@ -1,7 +1,3 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
 CREATE TABLE "living_cost_df" (
     "state" VARCHAR   NOT NULL,
     "cost_index" DECIMAL   NOT NULL,
@@ -26,3 +22,12 @@ REFERENCES "living_cost_df" ("state");
 ALTER TABLE "income_df" ADD CONSTRAINT "fk_income_df_state" FOREIGN KEY("state")
 REFERENCES "living_cost_df" ("state");
 
+SELECT * FROM homes_df;
+SELECT * FROM income_df;
+SELECT * FROM living_cost_df; --contains primary key
+
+SELECT homes_df.*, living_cost_df.cost_index, income_df.income FROM homes_df
+INNER JOIN living_cost_df
+ON homes_df.state = living_cost_df.state
+INNER JOIN income_df
+ON income_df.state = living_cost_df.state;
